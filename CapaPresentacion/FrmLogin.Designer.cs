c07c8b0a306f5225bd3ecfb876e8400d5084d744
Alexander García <kevider99@gmail.com>
2025-05-23 22:55:34 -0600

    diseño en el login

--- CapaPresentacion/FrmLogin.Designer.cs
+++ CapaPresentacion/FrmLogin.Designer.cs
@@ -24,52 +24,58 @@ namespace CapaPresentacion
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(30, 40);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 73);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 25);
+            this.label1.Size = new System.Drawing.Size(111, 34);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario:";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(30, 90);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 140);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.Size = new System.Drawing.Size(165, 33);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña:";
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(140, 40);
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(200, 73);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(200, 22);
+            this.txtUsuario.Size = new System.Drawing.Size(200, 34);
             this.txtUsuario.TabIndex = 2;
             // 
             // txtClave
             // 
-            this.txtClave.Location = new System.Drawing.Point(140, 90);
+            this.txtClave.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClave.Location = new System.Drawing.Point(200, 139);
             this.txtClave.Name = "txtClave";
             this.txtClave.PasswordChar = '*';
-            this.txtClave.Size = new System.Drawing.Size(200, 22);
+            this.txtClave.Size = new System.Drawing.Size(200, 34);
             this.txtClave.TabIndex = 3;
             // 
             // btnIngresar
             // 
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIngresar.IconChar = FontAwesome.Sharp.IconChar.SignInAlt;
             this.btnIngresar.IconColor = System.Drawing.Color.Green;
             this.btnIngresar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnIngresar.IconSize = 25;
+            this.btnIngresar.IconSize = 35;
             this.btnIngresar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnIngresar.Location = new System.Drawing.Point(140, 140);
+            this.btnIngresar.Location = new System.Drawing.Point(140, 211);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(200, 35);
+            this.btnIngresar.Size = new System.Drawing.Size(212, 51);
             this.btnIngresar.TabIndex = 4;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click_1);
             // 
             // FrmLogin
             // 
-            this.ClientSize = new System.Drawing.Size(400, 220);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(484, 288);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUsuario);
